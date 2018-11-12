a8bf19fefe185e82abffa67bbbab807dedf9e030
Ryan Mui <reachryanmui@gmail.com>
2018-11-12 01:20:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,8 @@ var playlist = new Object({song1: "Thanks"})
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, {[artistName]: "songTitle"})
 }
- function removeFromPlaylist(playlist, artistName) {
+
+function removeFromPlaylist(playlist, artistName) {
    delete playlist.artistName
    return playlist
 }
